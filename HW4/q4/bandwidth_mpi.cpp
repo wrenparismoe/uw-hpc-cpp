aa7d5199ddbc697550594eb7f9f0868e6682a595
Warren Paris-Moe <wrenparismoe@gmail.com>
2023-05-17 22:30:25 -0700

    updated broadcast_mpi.cpp

--- HW4/q4/bandwidth_mpi.cpp
+++ HW4/q4/bandwidth_mpi.cpp
@@ -6,21 +6,21 @@
 
 template <typename T>
 void my_broadcast(T* data, int count, int root, MPI_Comm comm) {
-  // Get the rank of the current process, and number of processes.
+    // Get the rank of the current process, and number of processes.
     int rank, size;
     MPI_Comm_rank(comm, &rank);
     MPI_Comm_size(comm, &size);
 
     // If the current process is not the root process, then receive the data from the root.
     if (rank != root) {
-      MPI_Recv(data, count, MPI_BYTE, root, 0, comm, MPI_STATUS_IGNORE);
+        MPI_Recv(data, count, MPI_BYTE, root, 0, comm, MPI_STATUS_IGNORE);
     } else {
-      // If the current process is the root process, then send the data to all other processes.
-      for (int i = 0; i < size; i++) {
-        if (i != root) {
-          MPI_Send(data, count, MPI_BYTE, i, 0, comm);
+        // If the current process is the root process, then send the data to all other processes.
+        for (int i = 0; i < size; i++) {
+            if (i != root) {
+                MPI_Send(data, count, MPI_BYTE, i, 0, comm);
+            }
         }
-      }
     }
 }
 
@@ -39,43 +39,50 @@ int main(int argc, char** argv) {
     // Create a vector to store the measurements.
     std::vector<double> my_bandwidth(61);
     std::vector<double> bcast_bandwidth(61);
+    int index = 0;
 
     // Loop over the message sizes.
-    for (int i = 0; i <= 12; i++) {
-      // Set the message size.
-      int message_size = 8 * (1 << i);
+    for (int i = 8; i <= 128*1024*1024; i*=2) {
 
-      // Create a buffer to store the data.
-      char* buffer = new char[message_size];
+        // Create a buffer to store the data.
+        char* buffer = new char[i];
 
-      // Initialize the data in the buffer.
-      for (int j = 0; j < message_size; j++) {
-        buffer[j] = 'a';
-      }
+        // Initialize the data in the buffer.
+        for (int j = 0; j < i; j++) {
+            buffer[j] = 'a';
+        }
 
-      double start_time = MPI_Wtime();
-      my_broadcast(buffer, message_size, 0, MPI_COMM_WORLD);
-      double end_time = MPI_Wtime();
-      my_bandwidth[i] = (message_size / (end_time - start_time)) / (1024 * 1024);
+        double start_time = MPI_Wtime();
+        my_broadcast(buffer, i, 0, MPI_COMM_WORLD);
+        double end_time = MPI_Wtime();
+        my_bandwidth[index] = (i / (end_time - start_time));
 
-      start_time = MPI_Wtime();
-      MPI_Bcast(buffer, message_size, MPI_BYTE, 0, MPI_COMM_WORLD);
-      end_time = MPI_Wtime();
-      bcast_bandwidth[i] = (message_size / (end_time - start_time)) / (1024 * 1024);
+        start_time = MPI_Wtime();
+        MPI_Bcast(buffer, i, MPI_BYTE, 0, MPI_COMM_WORLD);
+        end_time = MPI_Wtime();
+        bcast_bandwidth[index] = (i / (end_time - start_time));
 
-      // Delete the buffer.
-      delete[] buffer;
+        // Print message size, my_bandwidth, bcast_bandwidth for rank 0 at current iteration.
+        if (rank == 0) {
+            std::cout << "message_size: " << i << " Bytes | My Bandwidth: " << my_bandwidth[index] << " Bcast Bandwidth: " << bcast_bandwidth[index] << std::endl;
+        }
+
+        // Delete the buffer.
+        delete[] buffer;
+        index++;
     }
 
     // Write the measurements to a CSV file.
     if (rank == 0) {
-      std::ofstream csv_file("bandwidth.csv");
-      csv_file << "message_size,my_bandwidth,bcast_bandwidth" << std::endl;
-      for (int i = 0; i <= 12; i++) {
-        int message_size = 8 * (1 << i);
-        csv_file << message_size << "," << my_bandwidth[i] << "," << bcast_bandwidth[i] << std::endl;
-      }
-      csv_file.close();
+        std::ofstream csv_file("bandwidth.csv");
+        csv_file << "message_size,my_bandwidth,bcast_bandwidth" << std::endl;
+        index = 0;
+        for (int i = 8; i <= 128*1024*1024; i*=2) {
+            int message_size = 8 * (1 << i);
+            csv_file << message_size << "," << my_bandwidth[index] << "," << bcast_bandwidth[index] << std::endl;
+            index += 1;
+        }
+        csv_file.close();
     }
 
     // Finalize MPI.
